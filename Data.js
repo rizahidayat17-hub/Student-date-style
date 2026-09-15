3eaf1e2a8f271d06375d25a0b56a5883148c00a4
Riza Nur Hidayat <rizanurhidayat19@gmail.com>
2026-09-15 16:34:30 +0700

    Create data.js

---
+++ Data.js
@@ -0,0 +1,6 @@
+const dataMahasiswa = [
+  { id: 1, nama: "Budi Santoso", nim: "12345678", jurusan: "Teknik Informatika" },
+  { id: 2, nama: "Siti Rahma", nim: "23456789", jurusan: "Sistem Informasi" },
+  { id: 3, nama: "Ahmad Dahlan", nim: "34567890", jurusan: "Teknik Komputer" },
+  { id: 4, nama: "Dewi Lestari", nim: "45678901", jurusan: "Informatika" }
+];
